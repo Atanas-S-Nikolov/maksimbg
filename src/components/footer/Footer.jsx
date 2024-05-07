b5da5e6d0c06975728218f7bc6529f24ad4c0554
Atanas-S-Nikolov <atanas.stoyanov1199@gmail.com>
2024-05-07 14:16:05 +0300

    Fix footer layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,14 +2,55 @@ import styles from "@/styles/components/footer/Footer.module.css";
 
 import Typography from "@mui/material/Typography";
 
+import Icon from "@mui/material/Icon";
+
 import { SITE_NAME } from "@/constants/global";
+import Link from "next/link";
+import { useMediaQuery } from "@react-hookz/web";
+
+const EMAIL = "Имейл";
+const PHONE = "Телефон за връзка";
+
+const contacts = [
+  {
+    icon: "phone_android",
+    text: PHONE,
+    value: "+359 87 965 5846",
+  },
+  { icon: "mail", text: EMAIL, value: "maksim_asparuhov@abv.bg" },
+];
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
+  const mobileS = useMediaQuery("(max-width: 321px)", {
+    initializeWithValue: false,
+  });
+  const tablet = useMediaQuery("(max-width: 750px)", {
+    initializeWithValue: false,
+  });
+  const iconFontSize = mobileS ? "medium" : "large";
+  const copyrightTextFontSize = tablet ? "small" : "medium";
 
   return (
     <footer className={styles.footer}>
-      <Typography color="text.secondary">&copy; {currentYear} {SITE_NAME}. Всички права запазени.</Typography>
+      <section className={styles.contacts}>
+        {contacts.map((contact, index) => {
+          const { icon, text, value } = contact;
+          const linkPrefix = text === EMAIL ? "mailto:" : "tel:";
+          return (
+            <span key={index}>
+              <Icon className={styles.icon} fontSize={iconFontSize}>{icon}</Icon>
+              <Typography>{text}</Typography>
+              <Link href={linkPrefix + value} data-rel="external">
+                {value}
+              </Link>
+            </span>
+          );
+        })}
+      </section>
+      <Typography fontSize={copyrightTextFontSize}>
+        &copy; {currentYear} {SITE_NAME}. Всички права запазени.
+      </Typography>
     </footer>
-  )
+  );
 }
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="bg">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
